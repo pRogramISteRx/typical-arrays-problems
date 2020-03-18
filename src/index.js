7100cec6ba002fe2fd546c0e3610c8b9fb4a95d7
Vadim Kotkovets <kotkowec@gmail.com>
2020-03-18 23:59:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    } else {
+        let minElement = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (minElement > array[i]) {
+                minElement = array[i];
+            }
+        }
+        return minElement;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    } else {
+        let maxElement = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (maxElement < array[i]) {
+                maxElement = array[i];
+            }
+        }
+        return maxElement;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    } else {
+        let avgValue;
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        avgValue = sum / array.length;
+        return avgValue;
+    }
 }
