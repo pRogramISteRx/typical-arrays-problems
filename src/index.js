d2c3be55027a7c2107e359e800f3b7b5d02c2bd1
Вадим Котковец <kotkowec@gmail.com>
2020-09-29 16:16:33 +0300

    refactor: update solution

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 exports.min = function min (array) {
-    if (arguments.length === 0 || array.length === 0) {
+    if (!Array.isArray(array) || !array.length) {
         return 0;
     } else {
-        let minElement = array[0];
-        for (let i = 1; i < array.length; i++) {
-            if (minElement > array[i]) {
-                minElement = array[i];
+        let min = array[0];
+        for (let i = 1; i < array.length; i += 1) {
+            if (min > array[i]) {
+                min = array[i];
             }
         }
-        return minElement;
+        return min;
     }
 }
 
 exports.max = function max (array) {
-    if (arguments.length === 0 || array.length === 0) {
+    if (!Array.isArray(array) || !array.length) {
         return 0;
     } else {
-        let maxElement = array[0];
-        for (let i = 1; i < array.length; i++) {
-            if (maxElement < array[i]) {
-                maxElement = array[i];
+        let max = array[0];
+        for (let i = 1; i < array.length; i += 1) {
+            if (max < array[i]) {
+                max = array[i];
             }
         }
-        return maxElement;
+        return max;
     }
 }
 
 exports.avg = function avg (array) {
-    if (arguments.length === 0 || array.length === 0) {
+    if (!Array.isArray(array) || !array.length) {
         return 0;
     } else {
-        let avgValue;
         let sum = 0;
-        for (let i = 0; i < array.length; i++) {
+        let avg;
+        for (let i = 0; i < array.length; i += 1) {
             sum += array[i];
         }
-        avgValue = sum / array.length;
-        return avgValue;
+        avg = sum / array.length;
+        return avg;
     }
 }
